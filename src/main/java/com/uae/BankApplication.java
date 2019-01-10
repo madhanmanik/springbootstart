df0e569afa72e884115b0804d966b41e14c8e6f8
madhanmanik <madhanmanik@gmail.com>
2019-01-10 15:05:57 +0530

    flow

---
+++ src/main/java/com/uae/BankApplication.java
@@ -0,0 +1,14 @@
+package com.uae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	 public static void main(String[] args) throws Exception{
+         SpringApplication.run(BankApplication.class, args);
+     }
+
+
+}
---
+++ src/main/java/com/uae/controller/BankController.java
@@ -0,0 +1,19 @@
+package com.uae.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class BankController {
+
+	 @GetMapping("/test")
+	    public String getTransactionDetails(@RequestParam String name) {
+	        return name+"getTrn";
+	    }
+
+
+
+}
